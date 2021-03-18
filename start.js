f8ee42d88abef33b01522d20e4256a19504521d5
Ewerton <ewerton.azevedo@live.co.uk>
2021-03-18 13:01:46 +0100

    Add port env variable

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 
 const app = require('./app')
-app.set('port', 4444);
+app.set('port', process.env.PORT || 4444);
 const server = app.listen(app.get('port'), ()=>{
     console.log(`Express running: PORT ${server.address().port}`)
 })
